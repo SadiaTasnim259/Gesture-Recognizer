719f00470ebb143bc310811693fa436c910643b0
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-03-25 11:05:51 +0600

    Tap gesture recogniser

--- Gesture Recognizer/ViewController.swift
+++ Gesture Recognizer/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var foodImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        //foodImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        
+        foodImageView.addGestureRecognizer(gestureRecognizer)
     }
-
+  
+    @objc func changePic (){
+        if foodImageView.image == UIImage(named: "food1"){
+            foodImageView.image = UIImage(named: "food2")
+        } else {
+            foodImageView.image = UIImage(named: "food1")
+        }
+    }
 
 }
 
