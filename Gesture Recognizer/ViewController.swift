74bef0e4fb8a1b5466ffebebcb74f69a03924f80
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-03-25 09:02:40 +0600

    Initial Commit

---
+++ Gesture Recognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gesture Recognizer
+//
+//  Created by Sadia on 25/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
